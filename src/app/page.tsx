3e2b44c7b0d3b81664a54a565c380b0063a51e67
Joseph Walker <99849204+joewxlker@users.noreply.github.com>
2023-12-02 15:40:16 +0800

    add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import roadmap from "public/roadmap.json";
 import utility from "public/utility.json";
 import uuid4 from "uuid4";
 import { Animation } from "./animation";
+import Image from "next/image";
 
 
 export default function HomePage() {
@@ -21,10 +22,13 @@ export default function HomePage() {
       <main
         className={`flex min-h-screen flex-col items-center justify-center gap-5 text-black ${layout}`}
       >
-        <div id="landing" className="h-[600px] my-40 relative xl:px-20 lg:px-5 md:px-5 px-2 max-w-[2000px] w-screen">
+        <div id="landing" className="xl:py-40 lg:py-40 xl:pb-0 lg:pb-0 pb-40 relative xl:px-20 lg:px-5 md:px-5 px-2 max-w-[2000px] w-screen">
           <h1 className="text-6xl font-heading text-t1 p-5">WELCOME TO THE LOONY BIN</h1>
           <p className="text-xl font-body text-t2 p-5">Compiled in 236ms (519 modules)</p>
-          <p className="absolute bottom-0 right-20 -rotate-45 text-xl font-body text-t2 p-5">Compiled in 236ms (519 modules)<br/>Compiled in 236ms (519 modules)<br/>Compiled in 236ms (519 modules)<br/></p>
+          <p className="absolute bottom-0 xl:right-20 lg:right-20 right-0 -rotate-45 text-xl font-body text-t2 p-5">Compiled in 236ms (519 modules)<br/>Compiled in 236ms (519 modules)<br/>Compiled in 236ms (519 modules)<br/></p>
+          <div className="flex items-center justify-center">
+            <Image src='/logo-nobg-512px.png' alt='' height={300} width={300} />
+          </div>
         </div>
         <Section.Container id={whitepaper.title} direction="right">
           <Section.Header title={whitepaper.title} direction="right"/>
